301d7993cd6d0e2388944bfdf05b372e4b881a60
Vladimir Steshin <warheim24@gmail.com>
2022-05-24 15:16:46 +0300

    Photo download and log added

--- download.py
+++ download.py
@@ -1,57 +1,72 @@
 import requests
 import os
+from pprint import pprint
+import datetime
+import json
 
 
 class VKDownload:
 
     def __init__(self, token, version, vk_id):
         self.token = token
-        self.host = 'https://api.vk.com/method/'
+        self.host = "https://api.vk.com/method/"
         self.version = version
         self.id = vk_id
 
     def get_params(self):
         return {
-            'access_token': self.token,
-            'v': self.version,
-            'owner_id': self.id,
-            'album_id': 'profile',
-            'rev': '1',
-            'photo_sizes': '1',
-            'extended': '1',
-            'count': '5'
+            "access_token": self.token,
+            "v": self.version,
+            "owner_id": self.id,
+            "album_id": "profile",
+            "rev": "1",
+            "photo_sizes": "1",
+            "extended": "1",
+            "count": "5"
         }
 
     def get_photo_list(self):
-        url = f'{self.host}photos.get'
+        url = f"{self.host}photos.get"
         params = self.get_params()
         request = requests.get(url, params=params).json()
         return request
 
     def create_folder(self):
-        if not os.path.isdir('photos'):
-            os.mkdir('photos')
-        os.chdir('photos')
+        if not os.path.isdir("photos"):
+            os.mkdir("photos")
+        os.chdir("photos")
         path = os.getcwd()
         return path
 
-    def download_dict(self, json):
+    def get_download_dict(self, json):
         d_dict = {}
-        for item in json['response']['items']:
-            name = item['likes']['count']
-            date = item['date']
-            largest_type = ''
-            url = ''
+        for item in json["response"]["items"]:
+            name = str(item["likes"]["count"])
+            date = str(datetime.datetime.fromtimestamp(item["date"]).strftime("%B %d, %Y %I_%M_%S"))
+            largest_type = ""
+            url = ""
             pixels = 0
-            for size in item['sizes']:
-                multiply = int(size['height']) * int(size['width'])
+            for size in item["sizes"]:
+                multiply = int(size["height"]) * int(size["width"])
                 if multiply > pixels:
                     pixels = multiply
-                    largest_type = size['type']
-                    url = size['url']
+                    largest_type = size["type"]
+                    url = size["url"]
             if name in d_dict.keys():
-                d_dict[date] = [url, largest_type]
+                d_dict[name + " " + date] = [url, largest_type]
             else:
                 d_dict[name] = [url, largest_type]
         return d_dict
 
+    def download_and_log(self, d_dict, path):
+        log = []
+        for key, value in d_dict.items():
+            session = {"file_name": key + ".jpg", "size": value[1]}
+            with open(path + "\\" + key + ".jpg", "wb") as file:
+                collect = requests.get(value[0])
+                file.write(collect.content)
+                log.append(session)
+        os.chdir("../")
+        path = os.getcwd()
+        with open(path + "\\" + "log.json", 'w') as here:
+            json.dump(log, here, indent=1)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from download import VKDownload
+from pprint import pprint
 
-token = 'a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd'
-version = '5.131'
-vk_id = '552934290'
+token = "a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd"
+version = "5.131"
+vk_id = "552934290"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     download = VKDownload(token, version, vk_id)
     json_list = download.get_photo_list()
     path = download.create_folder()
-    download_dict = download.download_dict(json_list)
+    download_dict = download.get_download_dict(json_list)
+    log = download.download_and_log(download_dict, path)
